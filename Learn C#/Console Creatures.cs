16c63c7e590232be41c62dd19eca8a7369e8abee
Vebjørn <119741691+vebyorn@users.noreply.github.com>
2023-03-31 19:04:34 +0200

    Create Console Creatures.cs

---
+++ Learn C#/Console Creatures.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // Prints a cool ASCII creature.
+      Console.WriteLine("          .-.");
+      Console.WriteLine("         |o o|");
+      Console.WriteLine("         | 0 |");
+      Console.WriteLine("         |   |");
+      Console.WriteLine("         '~~~'");
+      // Names a cool ASCII creature.
+      Console.WriteLine("The Ghost of Static Type");
+     }
+  }
+}
